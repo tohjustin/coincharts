8da1ecf527931db461cdfde9ad25ac92b35619a0
Justin Toh <tohjustin@hotmail.com>
2017-07-31 22:18:30 -0700

    InfoBox: Add useBigMinusChar()

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
+const BIG_MINUS_SIGN = '\u2212';
+const MINUS_SIGN = '-';
 const PERCENTAGE_FORMAT_REGEX = /^(\+?)(-?)([\d+][.?][\d{0,2}]+)(%)$/;
 const PRICE_FORMAT_REGEX = /^(\+?)(-?\$?)([\d|,]+)(.*)$/;
+
+const useBigMinusChar = (str = '') => str.replace(RegExp(MINUS_SIGN, 'g'), BIG_MINUS_SIGN);
 
 function formatValue(str) {
   const regex = PERCENTAGE_FORMAT_REGEX.test(str) ? PERCENTAGE_FORMAT_REGEX : PRICE_FORMAT_REGEX;
@@ -19,7 +23,7 @@ const InfoBox = ({ label, value }) => {
     <div className="infobox">
       <div className="value">
         <span className="small-font plus-char">{plusChar}</span>
-        <span className="small-font">{dollarChar}</span>
+        <span className="small-font">{useBigMinusChar(dollarChar)}</span>
         <span className="large-font">{mainValue}</span>
         <span className="small-font">{superscriptValue}</span>
       </div>
