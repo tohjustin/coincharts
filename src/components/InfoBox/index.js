d8772ca3eeedd2f80ef43dacba64a088f20ab7cb
Justin Toh <tohjustin@hotmail.com>
2017-07-31 20:45:14 -0700

    InfoBox: Add formatValue()

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -3,20 +3,32 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
-const InfoBox = ({ label, value }) => (
-  <div className="infobox">
-    <div className="value">
-      <span className="small-font">$</span>
-      <span className="large-font">2,719</span>
-      <span className="small-font">.91</span>
+const PRICE_FORMAT_REGEX = /^(\D*)([\d|,]+)(.*)$/;
+
+function formatValue(str) {
+  const regex = PRICE_FORMAT_REGEX;
+  const [, dollarChar, mainValue, superscriptValue] = regex.exec(str);
+  return [dollarChar, mainValue, superscriptValue];
+}
+
+const InfoBox = ({ label, value }) => {
+  const [dollarChar, mainValue, superscriptValue] = formatValue(value);
+
+  return (
+    <div className="infobox">
+      <div className="value">
+        <span className="small-font">{dollarChar}</span>
+        <span className="large-font">{mainValue}</span>
+        <span className="small-font">{superscriptValue}</span>
+      </div>
+      <div className="label">{label}</div>
     </div>
-    <div className="label">{label}</div>
-  </div>
-);
+  );
+};
 
 InfoBox.propTypes = {
   label: PropTypes.string.isRequired,
-  value: PropTypes.string,
+  value: PropTypes.string.isRequired,
 };
 
 export default InfoBox;
