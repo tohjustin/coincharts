45d46399cadb3c2036a32d1c77757585f2c46c12
Justin Toh <tohjustin@hotmail.com>
2017-07-31 21:03:32 -0700

    InfoBox: Detect '+' character in formatValue()

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
-const PRICE_FORMAT_REGEX = /^(\D*)([\d|,]+)(.*)$/;
+const PRICE_FORMAT_REGEX = /^(\+?)(\$?)([\d|,]+)(.*)$/;
 
 function formatValue(str) {
   const regex = PRICE_FORMAT_REGEX;
-  const [, dollarChar, mainValue, superscriptValue] = regex.exec(str);
-  return [dollarChar, mainValue, superscriptValue];
+  const [, plusChar, dollarChar, mainValue, superscriptValue] = regex.exec(str);
+  return [plusChar, dollarChar, mainValue, superscriptValue];
 }
 
 const InfoBox = ({ label, value }) => {
-  const [dollarChar, mainValue, superscriptValue] = formatValue(value);
+  const [plusChar, dollarChar, mainValue, superscriptValue] = formatValue(value);
 
   return (
     <div className="infobox">
       <div className="value">
+        <span className="small-font plus-char">{plusChar}</span>
         <span className="small-font">{dollarChar}</span>
         <span className="large-font">{mainValue}</span>
         <span className="small-font">{superscriptValue}</span>
