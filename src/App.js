b7fd714f9972c30c1667b0c7f297905afae27f0c
Justin Toh <tohjustin@hotmail.com>
2017-08-01 21:46:28 -0700

    App: Update usage of sub-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 import InfoBox from './components/InfoBox';
+import Tabs from './components/Tabs';
+
+import './App.css';
 
 const INITIAL_STATE = {
-  label: 'bitcoin price',
-  value: '$2,719.91',
+  info: [
+    { label: 'bitcoin price', value: '$2,857.01' },
+    { label: 'SINCE AN HOUR AGO (USD)', value: '+$17.83' },
+    { label: 'SINCE AN HOUR AGO (%)', value: '+0.91%' },
+  ],
+  currency: [
+    'Bitcoin · $2,730.75',
+    'Ethereum · $225.19',
+    'Litecoin · $42.93',
+  ],
+  selectedCurrencyIndex: 0,
 };
 
 class App extends Component {
@@ -13,15 +25,29 @@ class App extends Component {
     this.state = INITIAL_STATE;
   }
 
-  render() {
-    const { label, value } = this.state;
+  handleOptionClick = (nextIndex) => {
+    this.setState({
+      selectedCurrencyIndex: nextIndex,
+    });
+  }
 
+  render() {
     return (
       <div className="App">
-        <InfoBox
-          label={label}
-          value={value}
-        />
+        <div>
+          <Tabs
+            options={this.state.currency}
+            selectedIndex={this.state.selectedCurrencyIndex}
+            handleOptionClick={this.handleOptionClick}
+          />
+        </div>
+        <div>
+          {
+            this.state.info.map(e => (
+              <InfoBox key={e.label} label={e.label} value={e.value} />
+            ))
+          }
+        </div>
       </div>
     );
   }
