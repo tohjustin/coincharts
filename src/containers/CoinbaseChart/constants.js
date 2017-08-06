2263b99a25a6de4cb797ef6b1ea20747600c8f9e
Justin Toh <tohjustin@hotmail.com>
2017-08-06 00:00:28 -0700

    constants: Add DURATION & PRICE_DATA_PATH

---
+++ src/containers/CoinbaseChart/constants.js
@@ -0,0 +1,16 @@
+// Time duration constants
+const DURATION = {
+  DAY: 'day',
+  HOUR: 'hour',
+  MONTH: 'month',
+  WEEK: 'week',
+  YEAR: 'year',
+};
+
+// Directory where price data (.json files) are stored
+const PRICE_DATA_PATH = './priceData';
+
+export {
+  DURATION,
+  PRICE_DATA_PATH,
+};
