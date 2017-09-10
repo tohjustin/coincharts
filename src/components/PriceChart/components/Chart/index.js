c845903f4cf7dcddb46991a72c7aa02937b107f7
Justin Toh <tohjustin@hotmail.com>
2017-09-09 22:29:20 -0700

    PriceChart: Add Chart component

---
+++ src/components/PriceChart/components/Chart/index.js
@@ -0,0 +1,141 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import isEqual from 'lodash.isequal';
+import { area as d3Area, line as d3Line } from 'd3-shape';
+import { easeCubicOut } from 'd3-ease';
+import { extent } from 'd3-array';
+import { interpolatePath } from 'd3-interpolate-path';
+import { scaleLinear, scaleTime } from 'd3-scale';
+import { select } from 'd3-selection';
+import 'd3-transition';
+
+const CHART_PADDING_TOP = 20;
+const DEFAULT_COLOR = { fill: '#FFEBC5', stroke: '#FFB119' };
+const TRANSITION = { duration: 500, ease: easeCubicOut };
+const INITIAL_STATE = {
+  disableTransition: false,
+  previousColor: DEFAULT_COLOR,
+  previousScaledData: [],
+  scaledData: [],
+};
+
+class Chart extends PureComponent {
+  static scaleData(data, height, width) {
+    const scalePriceToY = scaleLinear()
+      .range([height, CHART_PADDING_TOP])
+      .domain(extent(data, d => d.price));
+
+    const scaleTimeToX = scaleTime()
+      .range([0, width])
+      .domain(extent(data, d => d.time));
+
+    return data.map(({ price, time }) => ({
+      price: scalePriceToY(price),
+      time: scaleTimeToX(time),
+    }));
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      data: nextData,
+      height: nextHeight,
+      width: nextWidth,
+    } = nextProps;
+    const { color, width } = this.props;
+    const { scaledData } = this.state;
+
+    const nextScaledData = Chart.scaleData(nextData, nextHeight, nextWidth);
+    const previousScaledData = (scaledData.length > 0) ?
+      scaledData : nextScaledData.map(({ time }) => ({ price: nextHeight, time }));
+
+    this.setState({
+      disableTransition: (width !== nextWidth),
+      previousColor: color,
+      previousScaledData,
+      scaledData: nextScaledData,
+    });
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { data, height, width } = this.props;
+    const { data: nextData, height: nextHeight, width: nextWidth } = nextProps;
+
+    return (
+      !isEqual(data, nextData) ||
+      !isEqual(height, nextHeight) ||
+      !isEqual(width, nextWidth)
+    );
+  }
+
+  componentDidUpdate() {
+    const { color, height } = this.props;
+    const { previousColor, previousScaledData, scaledData, disableTransition } = this.state;
+    const chart = select(this.svgNode);
+    const transitionDuration = (disableTransition) ? 0 : TRANSITION.duration;
+
+    const area = d3Area()
+      .x(d => d.time)
+      .y0(height)
+      .y1(d => d.price);
+    const line = d3Line()
+      .x(d => d.time)
+      .y(d => d.price);
+
+    const previousAreaChart = area(previousScaledData);
+    const previousLineChart = line(previousScaledData);
+    const areaChart = area(scaledData);
+    const lineChart = line(scaledData);
+
+    chart
+      .selectAll('path')
+      .remove();
+
+    chart
+      .append('path')
+        .attr('class', 'Chart-area')
+        .attr('d', previousAreaChart)
+        .style('fill', previousColor.fill)
+      .transition()
+        .duration(transitionDuration)
+        .ease(TRANSITION.ease)
+        .attrTween('d', () => interpolatePath(previousAreaChart, areaChart))
+        .style('fill', color.fill);
+
+    chart
+      .append('path')
+        .attr('class', 'Chart-line')
+        .attr('d', previousLineChart)
+        .style('stroke', previousColor.stroke)
+      .transition()
+        .duration(transitionDuration)
+        .ease(TRANSITION.ease)
+        .attrTween('d', () => interpolatePath(previousLineChart, lineChart))
+        .style('stroke', color.stroke);
+  }
+
+  render() {
+    return (
+      <g ref={(node) => { this.svgNode = node; }} />
+    );
+  }
+}
+
+Chart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    price: PropTypes.number,
+    time: PropTypes.date,
+  })).isRequired,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  color: PropTypes.shape({
+    fill: PropTypes.string,
+    stroke: PropTypes.string,
+  }).isRequired,
+};
+
+export default Chart;
--- src/components/PriceChart/index.js
+++ src/components/PriceChart/index.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import isEqual from 'lodash.isequal';
-import { area as d3Area, line as d3Line } from 'd3-shape';
 import { extent } from 'd3-array';
-import { interpolatePath } from 'd3-interpolate-path';
-import { scaleLinear, scaleTime } from 'd3-scale';
-import { select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
 import 'd3-transition';
 
+import Chart from './components/Chart';
 import Cursor from './components/Cursor';
 
 import {
   HOVER_CONTAINER_WIDTH,
   DEFAULT_COLOR,
-  DEFAULT_TRANSITION,
-  DEFAULT_FUNCTION,
 } from './constants';
 import { formatCurrency } from '../../utils';
 import './index.css';
@@ -23,7 +19,6 @@ import './index.css';
 const ACTIVE_CURRENCY = 'usd';
 const CHART_PADDING_TOP = 20;
 const INITIAL_STATE = {
-  data: [],
   dimensions: {
     height: 0,
     width: 0,
@@ -32,11 +27,8 @@ const INITIAL_STATE = {
   hoveredDataPoint: {},
   hoverX: -1,
   hoverY: -1,
-  previousColor: DEFAULT_COLOR,
-  previousScaledData: [],
-  scaledData: [],
-  scalePriceToY: DEFAULT_FUNCTION,
-  scaleTimeToX: DEFAULT_FUNCTION,
+  scalePriceToY: undefined,
+  scaleTimeToX: undefined,
 };
 
 class PriceChart extends Component {
@@ -52,32 +44,14 @@ class PriceChart extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { data: nextData } = nextProps;
-    const { color } = this.props;
-    const { dimensions, scaledData } = this.state;
-
-    const scaleTimeToX = scaleTime()
-      .range([0, dimensions.width])
-      .domain(extent(nextData, d => d.time));
+    const { dimensions } = this.state;
 
     const scalePriceToY = scaleLinear()
       .range([dimensions.height, CHART_PADDING_TOP])
       .domain(extent(nextData, d => d.price));
 
-    const nextScaledData = nextData.map(({ price, time }) => ({
-      price: scalePriceToY(price),
-      time: scaleTimeToX(time),
-    }));
-
-    const previousScaledData = (scaledData.length > 0) ?
-      scaledData :
-      nextScaledData.map(({ time }) => ({ price: dimensions.height, time }));
-
     this.setState({
       data: nextData,
-      scaledData: nextScaledData,
-      previousScaledData,
-      previousColor: color,
-      scaleTimeToX,
       scalePriceToY,
     });
   }
@@ -101,86 +75,24 @@ class PriceChart extends Component {
     );
   }
 
-  componentDidUpdate() {
-    const { previousColor, previousScaledData, scaledData, dimensions } = this.state;
-    const { color, transition } = this.props;
-    const chart = select(this.svgNode);
-
-    const area = d3Area()
-      .x(d => d.time)
-      .y0(dimensions.height)
-      .y1(d => d.price);
-    const line = d3Line()
-      .x(d => d.time)
-      .y(d => d.price);
-
-    const previousAreaChart = area(previousScaledData);
-    const previousLineChart = line(previousScaledData);
-    const newAreaChart = area(scaledData);
-    const newLineChart = line(scaledData);
-
-    chart
-      .selectAll('path')
-      .remove();
-
-    chart
-      .append('path')
-        .attr('class', 'Chart-area')
-        .attr('d', previousAreaChart)
-        .style('fill', previousColor.fill)
-      .transition()
-        .duration(transition.duration)
-        .ease(transition.ease)
-        .attrTween('d', () => interpolatePath(previousAreaChart, newAreaChart))
-        .style('fill', color.fill);
-
-    chart
-      .append('path')
-        .attr('class', 'Chart-line')
-        .attr('d', previousLineChart)
-        .style('stroke', previousColor.stoke)
-      .transition()
-        .duration(transition.duration)
-        .ease(transition.ease)
-        .attrTween('d', () => interpolatePath(previousLineChart, newLineChart))
-        .style('stroke', color.stroke);
-
-    // shouldComponentUpdate() ensures the following setState() doesn't cause a uneccesary rerender
-    // eslint-disable-next-line react/no-did-update-set-state
-    this.setState({ previousScaledData: scaledData });
-  }
-
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize);
   }
 
   handleResize = () => {
-    const { data } = this.state;
+    const { data } = this.props;
     const { height, width } = this.chartSvgComponent.getBoundingClientRect();
     const dimensions = {
       height: Math.round(height),
       width: Math.round(width),
     };
 
-    const scaleTimeToX = scaleTime()
-      .range([0, dimensions.width])
-      .domain(extent(data, d => d.time));
-
     const scalePriceToY = scaleLinear()
       .range([dimensions.height, CHART_PADDING_TOP])
       .domain(extent(data, d => d.price));
 
-    const scaledData = data.map(({ price, time }) => ({
-      price: scalePriceToY(price),
-      time: scaleTimeToX(time),
-    }));
-
     this.setState({
-      data,
       dimensions,
-      scaledData,
-      previousScaledData: scaledData,
-      scaleTimeToX,
       scalePriceToY,
     });
   }
@@ -194,7 +106,8 @@ class PriceChart extends Component {
   }
 
   updateHoverPosition = (e) => {
-    const { data, dimensions, scalePriceToY } = this.state;
+    const { data } = this.props;
+    const { dimensions, scalePriceToY } = this.state;
     const hoverX =
       e.nativeEvent.clientX - this.chartSvgComponent.getBoundingClientRect().left;
 
@@ -231,6 +144,7 @@ class PriceChart extends Component {
 
   render() {
     const { dimensions, hoverX, hoverY, hovered } = this.state;
+    const { data, color } = this.props;
 
     return (
       <div className="Chart-container">
@@ -241,7 +155,12 @@ class PriceChart extends Component {
           onMouseLeave={this.hideHoverElements}
           onMouseMove={this.updateHoverPosition}
         >
-          <g ref={(node) => { this.svgNode = node; }} />
+          <Chart
+            height={dimensions.height}
+            width={dimensions.width}
+            data={data}
+            color={color}
+          />
           <Cursor
             height={dimensions.height}
             visible={hovered}
@@ -263,15 +182,10 @@ PriceChart.propTypes = {
     fill: PropTypes.string,
     stroke: PropTypes.string,
   }),
-  transition: PropTypes.shape({
-    duration: PropTypes.number,
-    ease: PropTypes.function,
-  }),
 };
 
 PriceChart.defaultProps = {
   color: DEFAULT_COLOR,
-  transition: DEFAULT_TRANSITION,
 };
 
 export default PriceChart;
