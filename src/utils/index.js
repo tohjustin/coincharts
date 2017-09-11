eada1cec4d303a0de48cb6f7e600fd7190f3f6e3
Justin Toh <tohjustin@hotmail.com>
2017-09-10 18:05:59 -0700

    utils: Refactor formatCurrency()

--- src/utils/index.js
+++ src/utils/index.js
@@ -25,13 +25,13 @@ function fetchSpotPrices(cryptocurrencyList, currency) {
 }
 
 /**
- * Adds the appropriate symbol & separators to `rawCurrencyString` based on the input `currencyCode`
- * @param {string} rawCurrencyString
+ * Adds the appropriate symbol & separators to `value` based on the input `currencyCode`
+ * @param {string} value
  * @param {string} currencyCode
  * @returns Formatted currency string
  */
-function formatCurrency(rawCurrencyString, currencyCode) {
-  return currencyFormatter.format(rawCurrencyString, {
+function formatCurrency(value, currencyCode) {
+  return currencyFormatter.format(value, {
     code: currencyCode.toUpperCase(),
   });
 }
