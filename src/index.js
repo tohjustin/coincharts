d75362784438b9cdac6d406d83be5a289f2f0b37
Justin Toh <tohjustin@hotmail.com>
2017-09-11 19:03:47 -0700

    style: Add CSS reset

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './reset.css';
 import './index.css';
 
 // eslint-disable-next-line no-undef
