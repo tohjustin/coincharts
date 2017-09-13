c9f3103b0ced086836c30f104903268f7d064f0c
Justin Toh <tohjustin@hotmail.com>
2017-09-12 23:45:52 -0700

    scripts: Fix updatePriceData.sh

--- scripts/updatePriceData.sh
+++ scripts/updatePriceData.sh
@@ -3,40 +3,40 @@
 # Save the results as .json files into a directory provided in the first argument ($1)
 DATA_DIR=${1-"./public/priceData"}
 
+# Create directory if it doesn't exist
+if [ ! -d $DATA_DIR ]; then
+  mkdir -p $DATA_DIR;
+fi
+
 # Check if argument provided is a valid directory
 if [ ! -d "$DATA_DIR" ]; then
   echo 'Error: You have provided an invalid directory!'
   exit
 fi
 
-# Create directory if it doesn't exist
-if [ ! -d $DATA_DIR ]; then
-  mkdir -p $DATA_DIR;
-fi
-
 # Bitcoin (BTC)
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=all > "$DATA_DIR/BTC-USD_all.json"
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=year > "$DATA_DIR/BTC-USD_year.json"
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=month > "$DATA_DIR/BTC-USD_month.json"
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=week > "$DATA_DIR/BTC-USD_week.json"
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=day > "$DATA_DIR/BTC-USD_day.json"
-curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=hour > "$DATA_DIR/BTC-USD_hour.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=all > "$DATA_DIR/btc-usd_all.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=year > "$DATA_DIR/btc-usd_year.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=month > "$DATA_DIR/btc-usd_month.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=week > "$DATA_DIR/btc-usd_week.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=day > "$DATA_DIR/btc-usd_day.json"
+curl https://www.coinbase.com/api/v2/prices/BTC-USD/historic\?period\=hour > "$DATA_DIR/btc-usd_hour.json"
 
 # Ethereum (ETH)
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=all > "$DATA_DIR/ETH-USD_all.json"
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=year > "$DATA_DIR/ETH-USD_year.json"
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=month > "$DATA_DIR/ETH-USD_month.json"
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=week > "$DATA_DIR/ETH-USD_week.json"
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=day > "$DATA_DIR/ETH-USD_day.json"
-curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=hour > "$DATA_DIR/ETH-USD_hour.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=all > "$DATA_DIR/eth-usd_all.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=year > "$DATA_DIR/eth-usd_year.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=month > "$DATA_DIR/eth-usd_month.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=week > "$DATA_DIR/eth-usd_week.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=day > "$DATA_DIR/eth-usd_day.json"
+curl https://www.coinbase.com/api/v2/prices/ETH-USD/historic\?period\=hour > "$DATA_DIR/eth-usd_hour.json"
 
 # Litecoin (LTC)
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=all > "$DATA_DIR/LTC-USD_all.json"
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=year > "$DATA_DIR/LTC-USD_year.json"
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=month > "$DATA_DIR/LTC-USD_month.json"
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=week > "$DATA_DIR/LTC-USD_week.json"
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=day > "$DATA_DIR/LTC-USD_day.json"
-curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=hour > "$DATA_DIR/LTC-USD_hour.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=all > "$DATA_DIR/ltc-usd_all.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=year > "$DATA_DIR/ltc-usd_year.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=month > "$DATA_DIR/ltc-usd_month.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=week > "$DATA_DIR/ltc-usd_week.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=day > "$DATA_DIR/ltc-usd_day.json"
+curl https://www.coinbase.com/api/v2/prices/LTC-USD/historic\?period\=hour > "$DATA_DIR/ltc-usd_hour.json"
 
 # Spot Prices (BTC, ETH, LTC)
-curl https://www.coinbase.com/api/v2/prices/USD/spot? > "$DATA_DIR/USD_spot.json"
+curl https://www.coinbase.com/api/v2/prices/USD/spot? > "$DATA_DIR/usd_spot.json"
