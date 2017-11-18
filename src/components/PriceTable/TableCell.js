8d8bd4d5cd9fe9ed7b7d10cd37fbc299eee2e34d
Justin Toh <tohjustin@hotmail.com>
2017-11-18 11:53:09 -0800

    PriceTable: Refactor file structure & consolidate CSS

--- src/components/PriceTable/TableCell.js
+++ src/components/PriceTable/TableCell.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { formatCurrency } from "../../../../utils";
+import { formatCurrency } from "../../utils";
 import "./index.css";
 
 const ACTIVE_CURRENCY = "usd";
--- src/components/PriceTable/index.js
+++ src/components/PriceTable/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { scan } from "d3-array";
 
-import TableCell from "./components/TableCell";
+import TableCell from "./TableCell";
 
 import "./index.css";
 
