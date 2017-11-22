faa46d5ef5fe4eb9f91e0ce6816c3fcd4b924b70
Justin Toh <tohjustin@hotmail.com>
2017-11-22 22:14:53 +0800

    store: Refactor price module

--- src/store/price/index.js
+++ src/store/price/index.js
@@ -1,6 +1,12 @@
+import { PriceActions, PriceActionTypes } from "./actions";
+import * as PriceSelectors from "./selectors";
 import PriceReducer from "./reducer";
 import PriceSaga from "./saga";
-import * as PriceSelectors from "./selectors";
-import * as PriceActions from "./actions";
 
-export { PriceReducer, PriceSelectors, PriceSaga, PriceActions };
+export {
+  PriceActionTypes,
+  PriceActions,
+  PriceReducer,
+  PriceSaga,
+  PriceSelectors
+};
