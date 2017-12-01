c46f3f3a801401c357b80bed3e4c9d366bdb7b28
Justin Toh <tohjustin@hotmail.com>
2017-12-01 22:35:29 +0800

    containers: Add <DurationTabs/>

---
+++ src/containers/DurationTabs/index.js
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { connect } from "react-redux";
+
+import Tabs from "../../components/Tabs";
+import { DEFAULT_PROPS, DURATION_LIST, PROPTYPES } from "../../constants";
+import { SettingsActions, SettingsSelectors } from "../../store/settings";
+
+const DurationTabs = ({ handleDurationChange, selectedDuration }) => {
+  const options = DURATION_LIST.reduce((accumulator, { key, codename }) => {
+    accumulator[key] = {
+      listKey: codename,
+      element: <span>{codename}</span>
+    };
+    return accumulator;
+  }, {});
+
+  return (
+    <Tabs
+      options={options}
+      onChange={handleDurationChange}
+      selectedKey={selectedDuration}
+    />
+  );
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleDurationChange: durationKey => {
+      dispatch(SettingsActions.selectDuration(durationKey));
+    }
+  };
+};
+
+function mapStateToProps(state) {
+  const selectedDuration = SettingsSelectors.getSelectedDuration(state);
+  return { selectedDuration };
+}
+
+DurationTabs.propTypes = {
+  selectedDuration: PROPTYPES.DURATION,
+  handleDurationChange: PropTypes.func.isRequired
+};
+
+DurationTabs.defaultProps = {
+  selectedDuration: DEFAULT_PROPS.DURATION
+};
+
+// Use named export for tests
+export {
+  DurationTabs as UnconnectedDurationTabs,
+  mapDispatchToProps,
+  mapStateToProps
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DurationTabs);
