6534c069c9a989d95e7d452a3c46e5be1ff1ce03
Justin Toh <tohjustin@hotmail.com>
2018-04-13 15:51:58 -0700

    components/Table: Add \"isLoading\" prop for determining updates

--- src/components/Table/__tests__/Table.test.js
+++ src/components/Table/__tests__/Table.test.js
@@ -3,47 +3,43 @@ import { render } from "react-testing-library";
 
 import Table from "../";
 
+const TEST_PROPS = {
+  cryptocurrencyLabel: "testCryptocurrencyLabel",
+  durationLabel: "testDurationLabel",
+  currency: "USD",
+  isLoading: false,
+  percentDifference: 0,
+  priceDifference: 0,
+  spotPrice: 0,
+};
+
 describe("<Table />", () => {
   it("renders without crashing", () => {
-    const props = {
-      cryptocurrencyLabel: "testCryptocurrencyLabel",
-      durationLabel: "testDurationLabel",
-      currency: "USD",
-      percentDifference: 0,
-      priceDifference: 0,
-      spotPrice: 0,
-    };
-
-    const { container } = render(<Table {...props} />);
+    const { container } = render(<Table {...TEST_PROPS} />);
     expect(container.firstChild).not.toBeNull();
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it("renders 3 Table Cells when `props.durationLabel` is defined", () => {
-    const props = {
-      cryptocurrencyLabel: "testCryptocurrencyLabel",
-      durationLabel: "testDurationLabel",
-      currency: "USD",
-      percentDifference: 0,
-      priceDifference: 0,
-      spotPrice: 0,
-    };
-
-    const { container } = render(<Table {...props} />);
+    const { container } = render(<Table {...TEST_PROPS} />);
     expect(container.firstChild.children).toHaveLength(3);
   });
 
   it("renders only 1 Table Cell when `props.durationLabel` is undefined", () => {
-    const props = {
-      cryptocurrencyLabel: "testCryptocurrencyLabel",
-      durationLabel: "",
-      currency: "USD",
-      percentDifference: 0,
-      priceDifference: 0,
-      spotPrice: 0,
-    };
-
-    const { container } = render(<Table {...props} />);
+    const { container } = render(<Table {...TEST_PROPS} durationLabel="" />);
     expect(container.firstChild.children).toHaveLength(1);
+  });
+
+  it("does not rerender when `props.isLoading` is true", () => {
+    const { container } = render(<Table {...TEST_PROPS} />);
+    const { textContent } = container.firstChild;
+
+    // Component should not rerender
+    render(<Table {...TEST_PROPS} percentDifference={1000} isLoading />, { container });
+    expect(container.firstChild.textContent).toEqual(textContent);
+
+    // Component should rerender
+    render(<Table {...TEST_PROPS} percentDifference={1000} />, { container });
+    expect(container.firstChild.textContent).not.toEqual(textContent);
   });
 });
