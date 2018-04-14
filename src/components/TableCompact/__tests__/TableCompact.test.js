2960fbe413b0d520d0468f82f107c92db372cb07
Justin Toh <tohjustin@hotmail.com>
2018-04-13 17:08:09 -0700

    components/TableCompact: Add "isLoading" prop for determining updates

--- src/components/TableCompact/__tests__/TableCompact.test.js
+++ src/components/TableCompact/__tests__/TableCompact.test.js
@@ -4,32 +4,29 @@ import { render } from "react-testing-library";
 
 import TableCompact from "../";
 
+const TEST_PROPS = {
+  onCryptocurrencyChange: jest.fn(),
+  selectedCryptocurrency: "BTC",
+  cryptocurrencies: [{ key: "BTC" }, { key: "BCH" }, { key: "ETH" }, { key: "LTC" }],
+  currency: "USD",
+  isLoading: false,
+  percentDifference: 9.99,
+  priceDifference: 99.99,
+  spotPrice: 1999.99,
+};
+
 describe("<TableCompact />", () => {
   it("renders without crashing", () => {
-    const props = {
-      onCryptocurrencyChange: jest.fn,
-      selectedCryptocurrency: "BTC",
-      cryptocurrencies: [{ key: "BTC" }, { key: "bch" }, { key: "eth" }, { key: "ltc" }],
-      currency: "USD",
-      percentDifference: 9.99,
-      priceDifference: 99.99,
-      spotPrice: 1999.99,
-    };
-
-    const { container } = render(<TableCompact {...props} />);
+    const { container } = render(<TableCompact {...TEST_PROPS} />);
     expect(container.firstChild).not.toBeNull();
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it("renders negative differences correctly", () => {
     const props = {
-      onCryptocurrencyChange: jest.fn,
-      selectedCryptocurrency: "BTC",
-      cryptocurrencies: [{ key: "BTC" }, { key: "BCH" }, { key: "ETH" }, { key: "LTC" }],
-      currency: "USD",
+      ...TEST_PROPS,
       percentDifference: -9.99,
       priceDifference: -99.99,
-      spotPrice: 1999.99,
     };
     const expected = `$${Math.abs(props.priceDifference)} (${Math.abs(props.percentDifference)}%)`;
 
@@ -38,33 +35,32 @@ describe("<TableCompact />", () => {
   });
 
   it("renders positive differences correctly", () => {
-    const props = {
-      onCryptocurrencyChange: jest.fn,
-      selectedCryptocurrency: "BTC",
-      cryptocurrencies: [{ key: "BTC" }, { key: "BCH" }, { key: "ETH" }, { key: "LTC" }],
-      currency: "USD",
-      percentDifference: 9.99,
-      priceDifference: 99.99,
-      spotPrice: 1999.99,
-    };
-    const expected = `$${Math.abs(props.priceDifference)} (${Math.abs(props.percentDifference)}%)`;
+    const expected = `$${Math.abs(TEST_PROPS.priceDifference)} (${Math.abs(TEST_PROPS.percentDifference)}%)`;
 
-    const { getByTestId } = render(<TableCompact {...props} />);
+    const { getByTestId } = render(<TableCompact {...TEST_PROPS} />);
     expect(getByTestId("price-details").textContent).toEqual(expected);
+  });
+
+  it("does not rerender when `props.isLoading` is true", () => {
+    const { container } = render(<TableCompact {...TEST_PROPS} />);
+    const { textContent } = container.firstChild;
+
+    // Component should not rerender
+    render(<TableCompact {...TEST_PROPS} percentDifference={1000} isLoading />, { container });
+    expect(container.firstChild.textContent).toEqual(textContent);
+
+    // Component should rerender
+    render(<TableCompact {...TEST_PROPS} percentDifference={1000} />, { container });
+    expect(container.firstChild.textContent).not.toEqual(textContent);
   });
 
   // TODO: Switch to "react-testing-library" once https://github.com/kentcdodds/react-testing-library/pull/48 is merged
   it("triggers `props.onChange` callback when option is selected", () => {
     const initialSelectedOption = "BTC";
-    const selectedOption = "ltc";
+    const selectedOption = "LTC";
     const props = {
-      onCryptocurrencyChange: jest.fn(),
+      ...TEST_PROPS,
       selectedCryptocurrency: initialSelectedOption,
-      cryptocurrencies: [{ key: "BTC" }, { key: "bch" }, { key: "eth" }, { key: "ltc" }],
-      currency: "USD",
-      percentDifference: 9.99,
-      priceDifference: 99.99,
-      spotPrice: 1999.99,
     };
 
     const wrapper = mount(<TableCompact {...props} />);
